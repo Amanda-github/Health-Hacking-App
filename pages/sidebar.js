b71c71bf34699f06e79b4dadd2cfd66ed3cb73d6
Your Name <Your Email>
2020-03-12 15:12:32 +0800

    stacked changed

---
+++ pages/sidebar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { AppRegistry, Image, StatusBar } from "react-native";
+import {
+  Button,
+  Text,
+  Container,
+  List,
+  ListItem,
+  Content,
+  Icon
+} from "native-base";
+
+const Routes = ["Home", "Profile", "Settings"];
+
+const SideBar = ({navigation}) => {
+  return (
+    <Container>
+      <Content>
+        <Image
+          source={{
+            uri:
+              "https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/drawer-cover.png"
+          }}
+          style={{
+            height: 120,
+            width: "100%",
+            alignSelf: "stretch",
+            position: "absolute"
+          }}
+        />
+        <Image
+          square
+          style={{
+            height: 80,
+            width: 70,
+            position: "absolute",
+            alignSelf: "center",
+            top: 20
+          }}
+          source={{
+            uri:
+              "https://www.nicepng.com/png/detail/139-1396003_satisfied-customer-png-download-happy-customer-icon-png.png"
+          }}
+        />
+        <List dataArray={Routes}
+        contentContainerStyle={{marginTop:120}}
+        renderRow={data=>{
+            return(
+                <ListItem button onPress={()=>navigation.navigate()}
+            )
+        }}
+
+      </Content>
+    </Container>
+  );
+};
+
+export default SideBar;
