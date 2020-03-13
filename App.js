7ea25d6d515d64e59277662c1323c7bfaa2df3a1
joowee <jooweelim@gmail.com>
2020-03-13 11:36:01 +0800

    fixed the quotation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from “react”;
-import { NavigationContainer } from “@react-navigation/native”;
-import MainStack from “./navigators/StackNavigation”;
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MainStack from "./navigators/StackNavigation";
 export default function App() {
   return (
     <NavigationContainer>
