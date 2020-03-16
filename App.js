79517c38aa56373d4dca0512925370ad68123b38
Amanda Lee <leesiewhwei96@gmail.com>
2020-03-16 18:05:07 +0800

    further update

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthenticationNavigator from "./navigators/StackNavigation";
 import DrawerNavigator from "./navigators/DrawerNavigation";
-// import Login from "./pages/Login";
-// import HomePage from "./pages/HomePage";
-// import SignUp from "./pages/Signup";
 
 export default function App() {
   const [login, setLogin] = useState(true);
@@ -14,28 +11,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// state = {
-//   isLoggedIn: falsew
-// };
-// export default function App() {
-//   if (this.state.login)
-//     return <Login onSignupPress={() => this.setLogin({ login: false })} />;
-//   else return <Login onLoginPress={() => this.setLogin({ login: true })} />;
-// }
-
-// export default function App() {
-//   const [login, setLogin] = useState(false);
-//   return (
-//     <>
-//       <Login />
-//       <NavigationContainer>
-//         {login ? (
-//           <SignUp onSignupPress={() => setLogin(false)} />
-//         ) : (
-//           <HomePage onLoginPress={() => setLogin(true)} />
-//         )}
-//       </NavigationContainer>
-//     </>
-//   );
-// }
