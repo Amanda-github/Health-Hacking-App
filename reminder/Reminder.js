71f388c4b576d05598de3d57b39c2cc8fc2fb8b8
Amanda Lee <leesiewhwei96@gmail.com>
2020-03-26 20:19:21 +0800

    uninstall react-native calendar reminder, switched to expo notifications

---
+++ reminder/Reminder.js
@@ -0,0 +1,4 @@
+import React, { useEffect } from "react";
+import { View, Text, Button, Platform } from "react-native";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
